2e1f21b9cc064fb7062c545401338968ef64a79a
tomohiro0310 <momonu1990@gmail.com>
2020-02-04 21:42:04 +0900

    fixed-automatic-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -81,7 +81,7 @@ $(function(){
       }
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
